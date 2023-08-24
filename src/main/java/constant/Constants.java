de92d9dd4242e661dac366822dc13bb468fa255f
pinkman007n <142975644+pinkman007n@users.noreply.github.com>
2023-08-24 08:48:44 +0800

    Update Constants.java

--- src/main/java/constant/Constants.java
+++ src/main/java/constant/Constants.java
@@ -10,10 +10,10 @@ public class Constants {
     public final static String ORACLE_INITIAL_SIZE_KEY = "oracleInitialSize";
     public final static String ORACLE_MAX_ACTIVE_KEY = "oracleMaxActive";
 
-    public final static String ORACLE_DRIVER = "oracle.jdbc.driver.OracleDriver";
-    public final static String ORACLE_URL_C = "jdbc:oracle:thin:@10.0.4.204:1521:pmsdb";
-    public final static String ORACLE_USER_C = "sys as sysdba";
-    public final static String ORACLE_PASSWORD_C = "Nhu_123";
+    public final static String ORACLE_DRIVER = "看文档";
+    public final static String ORACLE_URL_C = "看文档";
+    public final static String ORACLE_USER_C = "看文档";
+    public final static String ORACLE_PASSWORD_C = "看文档";
     public final static int ORACLE_INITIAL_SIZE = 10;
     public final static int ORACLE_MAX_ACTIVE = 50;
 }
